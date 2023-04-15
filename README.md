c3d2e0c69d7d6ef7a92646f98e3bb15c9656762d
stefan <extern.stefan.lackner3@volkswagen.de>
2023-04-15 22:07:43 +0200

    add display

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ this one implements a client to get it compiled add nordic bib in the menue
 
 [RF24](./ide_bib.png)
 
+[u8g2](./u8g2.png)
--- client/client.ino
+++ client/client.ino
@@ -1,18 +1,33 @@
 
 #include <SPI.h>
 #include "RF24.h"
+#include "U8g2lib.h"
+
+
+//U8G2_ST7920_128X64_1_SW_SPI u8g2(rotation, clock, data, cs [, reset])
+U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, 13, 11, 10, 8);
+ 
 RF24 radio(7, 8); // (CE, CSN)
+
 const byte address[6] = "1RF24"; // address / identifier
 void setup() {
   Serial.begin(115200);
+  u8g2.begin();
   radio.begin();
   radio.openReadingPipe(0,address); // set the address for pipe 0
   radio.startListening(); // set as receiver
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  u8g2.drawStr(0,20,"start");
+  u8g2.sendBuffer();
 }
 void loop() {
+  u8g2.clearBuffer();  
   if(radio.available()){
+    u8g2.setFont(u8g2_font_ncenB14_tr);
     char text[33] = {0}; 
     radio.read(&text, sizeof(text)-1);
+    u8g2.drawStr(0,20,text);
     Serial.println(text);
-  }
+    u8g2.sendBuffer();
+  }  
 }
