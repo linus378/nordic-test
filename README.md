f2da7beae6b4287a0310835c4ed2e13feb114d5e
stefan <extern.stefan.lackner3@volkswagen.de>
2023-04-15 18:37:58 +0200

    first try

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # nordic-test
+
+this one implements a client to get it compiled add nordic bib in the menue
+
+[RF24](./ide_bib.png)
+
--- client/client.ino
+++ client/client.ino
@@ -1,12 +1,18 @@
 
-
-
+#include <SPI.h>
+#include "RF24.h"
+RF24 radio(7, 8); // (CE, CSN)
+const byte address[6] = "1RF24"; // address / identifier
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  radio.begin();
+  radio.openReadingPipe(0,address); // set the address for pipe 0
+  radio.startListening(); // set as receiver
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(radio.available()){
+    char text[33] = {0}; 
+    radio.read(&text, sizeof(text)-1);
+    Serial.println(text);
+  }
 }
