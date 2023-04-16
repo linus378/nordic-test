30ca658f615ad6d5b7edd984bd474e9021ee980f
stefan <extern.stefan.lackner3@volkswagen.de>
2023-04-16 14:35:11 +0200

    sender

---
+++ sender/sender.ino
@@ -0,0 +1,15 @@
+#include <SPI.h>
+#include <RF24.h>
+RF24 radio(7, 8); // (CE, CSN)
+const byte address[6] = "1RF24"; // address / identifier
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address); // set the address
+  radio.stopListening(); // set as transmitter
+}
+void loop() {
+  const char text[] = "Hi Receiver"; // max. 32 bytes
+  radio.write(&text, sizeof(text));
+  
+  delay(2000);
+}
