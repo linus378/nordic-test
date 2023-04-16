433e5aae6f92f9d78ab4438faf981c291a5a06c0
stefan <extern.stefan.lackner3@volkswagen.de>
2023-04-16 14:50:53 +0200

    add counter

--- sender/sender.ino
+++ sender/sender.ino
@@ -1,6 +1,9 @@
 #include <SPI.h>
 #include <RF24.h>
-RF24 radio(4, 5); // (CE, CSN)
+
+long count_msg=0;
+
+RF24 radio(7, 8); // (CE, CSN)
 const byte address[6] = "1RF24"; // address / identifier
 void setup() {
   radio.begin();
@@ -8,8 +11,11 @@ void setup() {
   radio.stopListening(); // set as transmitter
 }
 void loop() {
-  const char text[] = "Hi Receiver"; // max. 32 bytes
-  radio.write(&text, sizeof(text));
-  
+  count_msg++;
+  String msg_str="Hi Receiver";
+  msg_str+=count_msg;
+  //const char text[] = "Hi Receiver"; // max. 32 bytes
+  //radio.write(&text, sizeof(text));
+  radio.write(&msg_str, sizeof(msg_str));
   delay(2000);
 }
