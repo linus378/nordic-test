a2d454d4bcc2abdc6ee27bfc4bb30630ebdaf49d
linus378 <llackner@fedora.fritz.box>
2023-04-16 15:19:40 +0200

    idk

--- sender/sender.ino
+++ sender/sender.ino
@@ -1,6 +1,8 @@
 #include <SPI.h>
 #include <RF24.h>
 
+long count_msg=0;
+
 RF24 radio(4, 5); // (CE, CSN)
 const byte address[6] = "1RF24"; // address / identifier
 void setup() {
@@ -9,9 +11,12 @@ void setup() {
   radio.stopListening(); // set as transmitter
 }
 void loop() {
-  unsigned long time = millis();
-  radio.write( &time, sizeof(unsigned long) );
-  //const char text[] = "Hi Receiver"; // max. 32 bytes
-  //radio.write(&text, sizeof(text));
+  count_msg++;
+  String msg_str="Hi Receiver ";
+  msg_str+=count_msg;
+  const char * msg_chr = msg_str.c_str();
+  const char text[] = "Hi Receiver"; // max. 32 bytes
+  radio.write(&text, sizeof(text));
+  //radio.write(&msg_chr, sizeof(msg_chr));
   delay(2000);
 }
