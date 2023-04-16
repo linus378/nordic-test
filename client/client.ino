ce4d69face7d78a3be1a875f2f700544a05e46d9
linus378 <llackner@fedora.fritz.box>
2023-04-16 14:38:10 +0200

    idk

--- client/client.ino
+++ client/client.ino
@@ -1,38 +1,15 @@
-
 #include <SPI.h>
-#include "RF24.h"
-#include <GyverOLED.h>
-
-
-GyverOLED<SSH1106_128x64> oled;
-
+#include <RF24.h>
 RF24 radio(4, 5); // (CE, CSN)
-
 const byte address[6] = "1RF24"; // address / identifier
 void setup() {
-  oled.init();  
-  oled.clear();   
-  oled.update(); 
-  oled.setCursorXY(28, 26);   
-  oled.print("init....");
-  oled.rect(0,0,127,63,OLED_STROKE);
-  oled.update();
-  Serial.begin(115200);
   radio.begin();
-  radio.openReadingPipe(0,address); // set the address for pipe 0
-  radio.startListening(); // set as receiver
-  delay(1000);
+  radio.openWritingPipe(address); // set the address
+  radio.stopListening(); // set as transmitter
 }
-
-void loop() { 
-  if(radio.available()){
-    oled.setCursorXY(28, 26);
-    char text[33] = {0}; 
-    radio.read(&text, sizeof(text)-1);
-    oled.print(text);
-    oled.rect(0,0,127,63,OLED_STROKE);
-    oled.update();
-    Serial.println(text);
-  }
-  delay(1000);  
+void loop() {
+  const char text[] = "Hi Receiver"; // max. 32 bytes
+  radio.write(&text, sizeof(text));
+  
+  delay(2000);
 }
