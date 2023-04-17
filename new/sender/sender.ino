bdc99191e4caf49452e2a0ae113e3d6b3f4b3f47
SL <ip_adress@gmx.de>
2023-04-17 14:58:25 +0200

    fix syntax

--- new/sender/sender.ino
+++ new/sender/sender.ino
@@ -2,17 +2,18 @@
 #include <RF24.h>
 #include <WebServer.h>
 #define PORT 80
-#include <arduino-timer.h>
+//#include <arduino-timer.h>
 
-const char* ssid = "LI-LAN";
-const char* pass = "o6$#@99#Qs*hNy@9R2HEXARQ5";
+//const char* ssid = "LI-LAN";
+//const char* pass = "o6$#@99#Qs*hNy@9R2HEXARQ5";
+
 
 WebServer server(80);
 
-auto timer = timer_create_default();
+//auto timer = timer_create_default();
 
-RF24 radio(4, 5); // (CE, CSN)
-
+//RF24 radio(4, 5); // (CE, CSN)
+RF24 radio(7, 8); // (CE, CSN)
 const byte address[6] = "1RF24"; // address / identifier
 
 String ant= "<a href=\"/ant_\"><button style=\"background: green; color: white; font-size: x-large; \">Sending Data ... </button></a>";
@@ -45,23 +46,36 @@ void _no_ant(){
 }
 
 void setup(){
+  Serial.begin(115200);
+  
   WiFi.begin(ssid, pass);
-  WiFi.status();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println(WiFi.localIP());
   
   server.on("/ant_", _ant );
   server.on("/no_ant", _no_ant  );
   server.on("/listening_", _listening );
   server.begin();
-  
-  radio.begin();
+  Serial.println("webserver online");
 
+  while ( radio.isChipConnected () != true )  {
+    delay(1000);
+    Serial.println("rf24 not connected yet..");
+  }
+  radio.begin();
   radio.setPALevel(RF24_PA_LOW); // sufficient for tests side by side 
   radio.openWritingPipe(address); // set the address
   radio.openReadingPipe(1,address);
   
-  }
+  Serial.println("leave setup");
+}
   
 void loop(){
+   //we must handle clients
+   server.handleClient();
    radio.startListening(); // set as receiver
   if(radio.available()){
 radio.read(&payload, sizeof(payload));
@@ -76,32 +90,34 @@ radio.read(&payload, sizeof(payload));
     message3 + ("Payload send" + String(payload));
 
     server.send(200, "text/html", message3);
-
+    /*
     oled.clear(); 
     oled.home();
     oled.print("No message");
-    oled.update()
+    oled.update();
+    */
 
  } 
- if(isChipConnected ()) {
+ if(radio.isChipConnected ()) {
    radio.stopListening();
    radio.write(&handshake, sizeof(handshake));
    radio.startListening();
 
-    String message1 = "";
-     message2 += ant;
+    String message2 = "";
+    message2 += ant;
     message2 += headAndTitle;
-    message2 += "Handshake send + String(handshake));
+    message2 += "Handshake send" + String(handshake);
     
     
     server.send(200, "text/html", message2);
-    
+    /*
     oled.clear(); 
     oled.home();
     oled.print("Sending Handshake");
     oled.update();
+    */
       
-}else(){
+}else{
 
     String message1 = "";
     message1 += headAndTitle;
@@ -109,12 +125,12 @@ radio.read(&payload, sizeof(payload));
     
     server.send(200, "text/html", message1);
     
-    oled.clear(); 
+    /*oled.clear(); 
     oled.home();
     oled.print("No message");
     oled.update();
-
-      delay(1000);
+    */
+    delay(1000);
 
 }
 
