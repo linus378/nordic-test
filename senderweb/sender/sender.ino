c40f189c0d16914fb76301038d282ffba0dc7aca
linus378 <llackner@fedora-fritz-box.fritz.box>
2023-04-17 03:22:01 +0200

    it finally works

--- senderweb/sender/sender.ino
+++ senderweb/sender/sender.ino
@@ -14,42 +14,65 @@ const byte address[6] = "1RF24"; // address / identifier
 
 String ant= "<a href=\"/ant_\"><button style=\"background: red; color: white; font-size: x-large; \">The Ant is connected</button></a>";
 
-
 String noant= "<a href=\"/no_ant\"><button style=\"background: red; color: white; font-size: x-large; \">Ant no no</button></a>";
+
+String headAndTitle = "<head><meta http-equiv=\"refresh\" content=\"2\"></head>"
+                      "<h1>Is Antenna Connected?</h1>"
+                      "Just look below</BR></BR>";
 
 
 void setup(){
   WiFi.begin(ssid, pass);
   WiFi.status();
 
-  server.on("/",handleRoot);
-  server.on("/ant_", _ant);
+  server.on("/", handleRoot );
+  server.on("/ant_", _ant );
+  server.on("/no_ant", _no_ant  );
   server.begin();
+  
   
   radio.begin();
   radio.openWritingPipe(address); // set the address
   radio.stopListening(); // set as transmitter
 }
 
+
+
+
   
 void loop(){
-  server.handleClient(); 
+  server.handleClient();
+}
+
+
+
+void sendandcheck(){
+if(radio.isChipConnected()){
+
+    String message = "";
+    message += headAndTitle;
+    message += ant;
+    server.send(200, "text/html", message);
+    const char text[] = "Hi Receiver"; // max. 32 bytes
+    radio.write(&text, sizeof(text));
+}
+else{
+  String message1 = "";
+    message1 += headAndTitle;
+    message1 += noant;
+    server.send(200, "text/html", message1);  
+}
+}
+
+
+void _ant(){
+ server.send(200, "text/html", ant);
+} 
+
+void _no_ant(){
+  server.send(200, "text/html", noant); 
 }
 
 void handleRoot() {
-  _ant();
-}
-
-void _ant(){
-  if(radio.isChipConnected()){
-  const char text[] = "Hi Receiver"; // max. 32 bytes
-  radio.write(&text, sizeof(text));
-  server.send(200, "text/html", ant); 
-  
-  
-  delay(2000);
-}
-else{
-  server.send(200, "text/html", noant); 
-}
+   sendandcheck();
 }
